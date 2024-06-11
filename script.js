7ca699932853aec837da73ba36c46937d5ccee1e
João Vitor <106831985+JoaoCorsete@users.noreply.github.com>
2024-06-11 16:18:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas= [{
-
-    enunciado: "Pergunta 1" ,
-    alternativas:[ "altenativa 1", "alternativa 2"
-]
-}]
+const perguntas= [
+   
+    {
+    enunciado: "existe quantas cores de casca de pitaya" ,
+          alternativas:[ "3",
+         "4"]
+        },
+        {
+            enunciado: "o que é o caju?" ,
+            alternativas:[ "fruta",
+             "pseudofruta"]
+            },
+            {
+            enunciado: "qual principal lanche de Maringá?" ,
+                alternativas:[ "hamburguer",
+                 "dogão"]
+                },
+                {
+    enunciado: "qual destes não é uma fruta?" ,
+    alternativas:[ "lichia",
+      "pinhão"]
+                },
+                {
+    enunciado: "em quanto tempo levou para uma pessoa comer uma pizza de 12 pedaços com garfo e faca?" ,
+                    alternativas:[ "aproximadamente 47 segundos",
+                      "aproximadamente 26 segundos"]
+                    }
+                ]
